f6aa8d220938dd49e5dc77e2a0dc4b39226cd368
octanewt <octanewt+git@protonmail.com>
2018-12-08 16:25:37 -0700

    Day 04 part 2

--- day04.py
+++ day04.py
@@ -9,7 +9,7 @@ date_format = r'%m-%d'
 min_format = r'%M'
 
 
-def part_one(in_file):
+def parse_input(in_file):
     events = {}
     with open(in_file, 'r') as fp:
         for line in fp:
@@ -54,6 +54,11 @@ def part_one(in_file):
                 gd = st
             guard_dates[cur_guard].append(gd)
 
+    return guard_dates, min_asleep
+
+
+def part_one(in_file):
+    guard_dates, min_asleep = parse_input(in_file)
     result = {}
     max_sleep = 0
     for guard in guard_dates:
@@ -80,9 +85,64 @@ def part_one(in_file):
 
 
 # result = part_one('input.txt')
-result = part_one('day04_01_input.txt')
+# result = part_one('day04_01_input.txt')
+#
+# for k, v in result.items():
+#     print('{}\t\t{}'.format(k, v))
+#
+# print('result\t\t{}'.format(int(result['guard']) * int(result['minute'])))
+
+# output
+# $ time python3 ./day04.py
+# guard           1987
+# minute          34
+# result          67558
+#
+# real    0m0.108s
+# user    0m0.000s
+# sys     0m0.000s
+
+
+def part_two(in_file):
+    guard_dates, min_asleep = parse_input(in_file)
+    result = {}
+    max_sleep = 0
+    for guard in guard_dates:
+        # print('#{}'.format(guard))
+        date_min_lists = [min_asleep.get(strftime(date_format, x), [])
+                          for x in guard_dates[guard]]
+        guard_max_sleep_count = 0
+        guard_minute = -1
+        guard_sleep_total = 0
+        for i, minute in enumerate(zip_longest(*date_min_lists, fillvalue='.')):
+            sleep_count = minute.count('#')
+            guard_sleep_total += sleep_count
+            # print('{}\t{}\t{}\t{}'.format(i, ''.join(minute), sleep_count, guard_sleep_total))
+            if sleep_count > guard_max_sleep_count:
+                guard_max_sleep_count = sleep_count
+                guard_minute = i
+
+        if guard_max_sleep_count > max_sleep:
+            max_sleep = guard_max_sleep_count
+            result['guard'] = guard
+            result['minute'] = guard_minute
+
+    return result
+
+
+# result = part_two('input.txt')
+result = part_two('day04_02_input.txt')
 
 for k, v in result.items():
     print('{}\t\t{}'.format(k, v))
 
 print('result\t\t{}'.format(int(result['guard']) * int(result['minute'])))
+
+# $ time python3 ./day04.py
+# guard           2633
+# minute          30
+# result          78990
+#
+# real    0m0.172s
+# user    0m0.000s
+# sys     0m0.015s
